ae62c226726c86e5f15f55384b973df6b7b3f843
icarop16 <pedroicaro161107@gmail.com>
2023-10-16 15:13:30 -0300

    Atividade Dolar

--- pedroicaro.py
+++ pedroicaro.py
@@ -1,9 +1,9 @@
+# Digite um valor e veja quantos dolares voce poderá comprar: R$
 real = float(input("Digite o Valor em Real: "))
 dolar = float(input("Digite a cotação do dia: "))
 conversão = real / dolar
 print(f"""
     Seu Valor em R$ {real}
-    Cotação atual em $ {dolar}
-
+    Cotação atual em $ {dolar} 
     Valor de Convesão $ {conversão}
 """)
